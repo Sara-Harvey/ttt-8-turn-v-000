88d20deaf7a2d0010de56711b4871e3df6ecffa3
Sara Harvey <saraharveynyc@gmail.com>
2018-08-17 14:40:29 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,7 @@ end
 def turn(board, index, value="X")
   if valid_move?(board, index) == true
 	puts move(board, index, value="X") 
-	&& display_board(board)
+	puts display_board(board)
   else
    if valid_move(board, index) == false
     puts turn
